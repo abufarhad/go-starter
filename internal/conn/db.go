0db5fc47809639b47dca0fe34196ae7a9a1dce36
abufarhad <abu.farhad@monstar-lab.com>
2023-06-12 17:00:50 +0600

    db config issue resolved

--- internal/conn/db.go
+++ internal/conn/db.go
@@ -38,6 +38,7 @@ func ConnectDb() error {
 
 	//Mysql connection
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	fmt.Println("dsn = ", dsn)
 	dB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		PrepareStmt: true,
 		Logger:      gormlogger.Default.LogMode(logMode),
