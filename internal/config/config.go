a3b215835b280595f0130fd0cf9460d420ef3fd4
abufarhad <abu.farhad@monstar-lab.com>
2023-06-12 17:33:39 +0600

    config struct modified

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,28 +6,6 @@ import (
 	_ "github.com/spf13/viper/remote"
 )
 
-type AppConfig struct {
-	App struct {
-		Name  string
-		Port  string
-		Page  int64
-		Limit int64
-		Sort  string
-	}
-
-	Db struct {
-		Host            string
-		Port            string
-		User            string
-		Pass            string
-		Schema          string
-		MaxIdleConn     int
-		MaxOpenConn     int
-		MaxConnLifetime time.Duration
-		Debug           bool
-	}
-}
-
 type App struct {
 	Name  string
 	Port  string
